8ff6bd7749eaccdfe9f9e941c4d4c45d1f918a05
Samrat Jindal <smrat.jindal@gmail.com>
2023-04-30 08:27:22 +0530

    Initial commit

---
+++ src/test/java/com/jindal/Springboottodoapplication/SpringBootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jindal.Springboottodoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
